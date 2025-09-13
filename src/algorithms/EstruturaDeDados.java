70a369d7c3e957c14cead3cb94d97bf0a7affff1
Lucas Aioria Serpa <136469417+LucasAioriaSerpa@users.noreply.github.com>
2025-09-13 18:30:17 -0300

    Update EstruturaDeDados.java

--- src/algorithms/EstruturaDeDados.java
+++ src/algorithms/EstruturaDeDados.java
@@ -2,6 +2,17 @@ package algorithms;
 
 import ImageInterpreter.Pixel;
 
+/**
+ * <h1>EstruturaDeDados</h1>
+ * <p>Interface que define os métodos essenciais para estruturas de dados que armazenam objetos do tipo Pixel.</p>
+ *
+ * <h2>Métodos</h2>
+ * <ul>
+ *     <li>{@code adicionar(Pixel p)}: Adiciona um pixel à estrutura de dados.</li>
+ *     <li>{@code remover()}: Remove e retorna um pixel da estrutura de dados.</li>
+ *     <li>{@code estaVazia()}: Verifica se a estrutura de dados está vazia.</li>
+ * </ul>
+ * */
 public interface EstruturaDeDados {
     void adicionar(Pixel p);
     Pixel remover();
