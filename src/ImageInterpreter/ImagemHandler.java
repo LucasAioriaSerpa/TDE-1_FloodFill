cca56640017a076c14a3662972e4525aee11ca59
Lucas Aioria Serpa <136469417+LucasAioriaSerpa@users.noreply.github.com>
2025-09-13 17:13:33 -0300

    Refactor pixel and image data structures to use HashMap
    Changed Pixel and ImagemHandler classes to use HashMap for storing position, color, and image dimensions instead of arrays. Added detailed logging for pixel creation and extraction processes to improve traceability and debugging.

--- src/ImageInterpreter/ImagemHandler.java
+++ src/ImageInterpreter/ImagemHandler.java
@@ -7,16 +7,28 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+
+import static javax.swing.UIManager.put;
 
 public class ImagemHandler {
     private final LoggingManager logger = new LoggingManager();
     private String filePath;
     private BufferedImage image;
+    private final HashMap<String, Integer> dimensions;
     private ArrayList<Pixel> pixels;
     public ImagemHandler(String filePath) throws IOException {
+        logger.logInfo(
+                "IMG-000",
+                String.format("Loading image from path: %s", filePath)
+        );
         this.filePath = filePath;
         File fileImage = new File(filePath);
         this.image = ImageIO.read(fileImage);
+        this.dimensions = new HashMap<String, Integer>();
+        this.dimensions.put("X", image.getWidth());
+        this.dimensions.put("Y", image.getHeight());
+        this.dimensions.put("totalPixels", image.getWidth() * image.getHeight());
         this.pixels = getPixels();
     }
     //? GETTERS AND SETTERS:
@@ -29,8 +41,8 @@ public class ImagemHandler {
     private ArrayList<Pixel> getPixels() {
         ArrayList<Pixel> pixels = new ArrayList<>();
         try {
-            for (int Y = 0; Y < image.getHeight(); Y++) {
-                for (int X = 0; X < image.getWidth(); X++) {
+            for (int Y = 0; Y < dimensions.get("Y"); Y++) {
+                for (int X = 0; X < dimensions.get("X"); X++) {
                     int RGB = image.getRGB(X, Y);
                     int RED = (RGB >> 16) & 0xFF;
                     int GREEN = (RGB >> 8) & 0xFF;
@@ -38,7 +50,7 @@ public class ImagemHandler {
                     Pixel pixel = new Pixel(X, Y, RED, GREEN, BLUE);
                     pixels.add(pixel);
                     logger.logInfo(
-                            "IMG-000",
+                            "IMG-100",
                             String.format(
                                     "Pixel at position (%d, %d) with color (R:%d, G:%d, B:%d) extracted successfully.",
                                     X, Y, RED, GREEN, BLUE
@@ -48,11 +60,19 @@ public class ImagemHandler {
             }
         } catch (Exception exception) {
             logger.logError(
-                    "IMG-001",
+                    "IMG-404",
                     "Error while extracting pixels from image.",
                     exception
             );
             throw new RuntimeException(exception);
+        } finally {
+            logger.logInfo(
+                    "IMG-200",
+                    String.format(
+                            "All %d pixels extracted successfully.",
+                            dimensions.get("totalPixels")
+                    )
+            );
         }
         return pixels;
     }
--- src/ImageInterpreter/Pixel.java
+++ src/ImageInterpreter/Pixel.java
@@ -1,15 +1,33 @@
 package ImageInterpreter;
 
+import utils.LoggingManager;
+
+import java.util.HashMap;
+
 public class Pixel {
-    private int[] position;
-    private int[] color;
+    private HashMap<String, Integer> position;
+    private HashMap<String, Integer> color;
     public Pixel(int x, int y, int red, int green, int blue) {
-        this.position = new int[]{x, y};
-        this.color = new int[]{red, green, blue};
+        LoggingManager loggingManager = new LoggingManager();
+        loggingManager.logInfo(
+                "PIX-000",
+                String.format(
+                        "Creating pixel at position (%d, %d) with color (R:%d, G:%d, B:%d).",
+                        x, y, red, green, blue
+                )
+        );
+        this.position = new HashMap<String, Integer>();
+        this.color = new HashMap<String, Integer>();
+        this.position.put("X", x);
+        this.position.put("Y", y);
+        this.color.put("R", red);
+        this.color.put("G", green);
+        this.color.put("B", blue);
+
     }
     //? GETTERS AND SETTERS:
-    public int[] getPosition() { return position; }
-    public void setPosition(int[] position) { this.position = position; }
-    public int[] getColor() { return color; }
-    public void setColor(int[] color) { this.color = color; }
+    public HashMap<String, Integer> getPosition() { return position; }
+    public void setPosition(HashMap<String, Integer> position) { this.position = position; }
+    public HashMap<String, Integer> getColor() { return color; }
+    public void setColor(HashMap<String, Integer> color) { this.color = color; }
 }
