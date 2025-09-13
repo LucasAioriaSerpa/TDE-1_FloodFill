bc7e6fee0652d8daf635daaef4f735b705ce0102
Lucas Aioria Serpa <136469417+LucasAioriaSerpa@users.noreply.github.com>
2025-09-13 17:00:59 -0300

    hotFix: imports pixel

--- src/algorithms/EstruturaDeDados.java
+++ src/algorithms/EstruturaDeDados.java
@@ -1,5 +1,7 @@
 package algorithms;
 
+import ImageInterpreter.Pixel;
+
 public interface EstruturaDeDados {
     void adicionar(Pixel p);
     boolean estaVazia();
--- src/algorithms/Fila.java
+++ src/algorithms/Fila.java
@@ -1,5 +1,7 @@
 package algorithms;
 
+import ImageInterpreter.Pixel;
+
 import java.util.LinkedList;
 
 public class Fila implements EstruturaDeDados{
